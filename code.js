656b69f438b0b940a71c97faa6196d65b3ce4ea8
kenzieadmin <kenzieadmin@users.noreply.github.com>
2023-04-26 05:35:13 +0000

    deploy: 04edc6d112672c50b5f27bdc24507a958b860de0

--- code.js
+++ code.js
@@ -2,3 +2,19 @@
 // and select "Open with Live Server"
 
 // Your Code Here.
+let submitButton = document.getElementById("submitButton");
+
+let submitForm = function(event) {
+    let nameInput = document.getElementById("nameInput");
+    let emailInput = document.getElementById("emailInput");
+    let textInput = document.getElementById("textInput");
+
+    let contactName = nameInput.value;
+    let emailAddress = emailInput.value;
+    let userMessage = textInput.value;
+
+    event.preventDefault();
+    alert(`Thank you ${contactName} for reaching out! We will reach back out to your email at ${emailAddress} as soon as possible regarding your message: \n\n"${userMessage}"`)
+}
+
+submitButton.addEventListener("click", submitForm);
